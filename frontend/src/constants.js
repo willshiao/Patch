60ef66075181b42a4e27666fa9c9d08aa218139c
ericong18 <ericong18@gmail.com>
2020-04-25 16:08:53 -0700

    Dynamically change color and assets in upload page

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,9 @@
-export const BASE_URL = "url";
+export const BASE_URL = "http://ffa09537.ngrok.io";
+
+export const uploadStyles = {
+  backgroundColor: {
+    success: "#eafeef",
+    normal: "#dfeeff",
+    disabled: "#f0f3f6"
+  },
+}

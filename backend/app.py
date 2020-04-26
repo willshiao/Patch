efdd4fc3f8584ff69cf391e04d5f2bc48737ac09
William Shiao <willshiao@gmail.com>
2020-04-25 22:11:27 -0700

    Add comments

--- backend/app.py
+++ backend/app.py
@@ -22,15 +22,17 @@ app.secret_key = "secret key"
 ALLOWED_EXTENSIONS = set(['mp4', 'm4a', 'mp3'])
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Gets audio track from a video file
 def extract_audio(vfile):
-	stream = ffmpeg.input(vfile)
-	audioname = str(uuid.uuid4()) + '.mp3'
-	out = ffmpeg.output(stream.audio, 'output/' + audioname)
-	out.run(overwrite_output=True)
-	return audioname
+    stream = ffmpeg.input(vfile)
+    audioname = str(uuid.uuid4()) + '.mp3'
+    out = ffmpeg.output(stream.audio, 'output/' + audioname)
+    out.run(overwrite_output=True)
+    return audioname
 
+# Removes high and low freq components
 def clean_track(track, samp = 22050//4):
     ff_track = fft(track)
     freq_track = np.fft.fftfreq(len(track), d=1/samp)
@@ -38,48 +40,57 @@ def clean_track(track, samp = 22050//4):
     recon_track = np.real(ifft(ff_track))
     return recon_track
 
-def sim(main, query, sr = 22050//4):
-# main is good
-#     batch_sz = sr * 10
+def sim(main, query, sr=22050//4):
+    # main is good track
     batch_sz = len(main) // 10
-#     idxs, dists = mts.mass2_batch(main, query, batch_size=len(query), top_matches=len(main) // len(query) - 1, n_jobs=16)
     idxs, dists = mts.mass2_batch(main, query, batch_size=batch_sz, top_matches=len(main) // batch_sz - 1, n_jobs=6)
     return idxs[np.argsort(dists)]
 
 def patch(filenames):
-	vid = filenames[0]
-	aud = filenames[1]
-	extracted_name = extract_audio(os.path.join(app.config['UPLOAD_FOLDER'], vid))
+    vid = filenames[0]
+    aud = filenames[1]
+    extracted_name = extract_audio(os.path.join(app.config['UPLOAD_FOLDER'], vid))
+    # Read at low sample rate
+    x, sr = lr.core.load('output/' + extracted_name, sr=22050//4)
+    good_x, good_sr = lr.core.load(os.path.join(app.config['UPLOAD_FOLDER'], aud), sr=sr)
 
-	x, sr = lr.core.load('output/' + extracted_name, sr=22050//4)
-	good_x, good_sr = lr.core.load(os.path.join(app.config['UPLOAD_FOLDER'], aud), sr=sr)
-	recon_good = clean_track(good_x)
-	recon_x = clean_track(x)
-	idxs = sim(recon_good, recon_x[40*sr:45*sr])
-	offset = idxs[0] - 40*sr
-	x_dur = len(x) / sr
-	clean_x, clean_sr = lr.core.load(os.path.join(app.config['UPLOAD_FOLDER'], aud), sr=48000)
-	start_clean = int(np.round(offset / sr * clean_sr))
-	end_clean = start_clean + int(np.round(x_dur * clean_sr))
-	clean_trimmed = clean_x[start_clean:end_clean + 1]
-	trimmed_name = str(uuid.uuid4()) + '.wav'
-	sf.write('output/' + trimmed_name, clean_trimmed, clean_sr, 'PCM_16')
+    # Remove high and low freqs
+    recon_good = clean_track(good_x)
+    recon_x = clean_track(x)
 
-	vfile = os.path.join(app.config['UPLOAD_FOLDER'], vid)
-	stream = ffmpeg.input(vfile)
-	stream2 = ffmpeg.input('output/' + trimmed_name)
-	output_video_name = str(uuid.uuid4()) + '.mp4'
-	out = ffmpeg.output(stream.video, stream2.audio, 'output/' + output_video_name)
-	out.run(overwrite_output=True)
-	print("done patching")
+    # Perform MASS
+    # TODO: randomize start and end indices and do majority voting
+    OFF_START = 40
+    OFF_END = 45
+    idxs = sim(recon_good, recon_x[OFF_START*sr:OFF_END*sr])
+    offset = idxs[0] - OFF_START*sr
+    offset_dur = offset / sr
+    x_dur = len(x) / sr
 
+    # Load in a high quality version
+    clean_x, clean_sr = lr.core.load(os.path.join(app.config['UPLOAD_FOLDER'], aud), sr=48000)
+    start_clean = int(np.round(offset / sr * clean_sr))
+    end_clean = start_clean + int(np.round(x_dur * clean_sr))
+    clean_trimmed = clean_x[start_clean:end_clean + 1]
 
+    # Save cleaned file as a wav
+    trimmed_name = str(uuid.uuid4()) + '.wav'
+    sf.write('output/' + trimmed_name, clean_trimmed, clean_sr, 'PCM_16')
+
+    # Patch video
+    vfile = os.path.join(app.config['UPLOAD_FOLDER'], vid)
+    stream = ffmpeg.input(vfile)
+    stream2 = ffmpeg.input('output/' + trimmed_name)
+    output_video_name = str(uuid.uuid4()) + '.mp4'
+    out = ffmpeg.output(stream.video, stream2.audio, 'output/' + output_video_name)
+    out.run(overwrite_output=True)
+    print("done patching")
 
 
 
 @app.route('/')
 def upload_form():
-	return render_template('upload.html')
+    return render_template('upload.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
